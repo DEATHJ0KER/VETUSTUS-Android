0ed592344b80660038492b8714543fd05d5e2a36
Valentino Decaroli <caronte.ade@gmail.com>
2026-09-13 23:23:39 +0200

    Register first app use with vxd.mobi

--- app/src/main/java/mobi/vxd/vetustus/micro/VetustusMicroApp.kt
+++ app/src/main/java/mobi/vxd/vetustus/micro/VetustusMicroApp.kt
@@ -1,22 +1,29 @@
 package mobi.vxd.vetustus.micro
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import mobi.vxd.vetustus.micro.data.AppDatabase
 import mobi.vxd.vetustus.micro.data.DownloadRepository
 import mobi.vxd.vetustus.micro.data.LibraryRepository
 import mobi.vxd.vetustus.micro.data.SettingsRepository
 import mobi.vxd.vetustus.micro.network.NetworkDirectory
 import mobi.vxd.vetustus.micro.network.XdccSearchClient
+import mobi.vxd.vetustus.micro.registration.FirstRunRegistration
 import mobi.vxd.vetustus.micro.storage.MediaArchiveManager
 import mobi.vxd.vetustus.micro.storage.MediaStorePublisher
 
 class VetustusMicroApp : Application() {
     val container: AppContainer by lazy { AppContainer(this) }
+    private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     override fun onCreate() {
         super.onCreate()
         container.database.recoverInterruptedTransfers()
         container.downloads.refresh()
+        appScope.launch { container.registration.registerIfNeeded() }
     }
 }
 
@@ -29,4 +36,5 @@ class AppContainer(application: Application) {
     val searchClient = XdccSearchClient()
     val publisher = MediaStorePublisher(application)
     val archiveManager = MediaArchiveManager(application, publisher, library)
+    val registration = FirstRunRegistration(application)
 }
