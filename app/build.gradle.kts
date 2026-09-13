8feb9e7b5acb4b421bb907c3d3894e100a7013d9
Valentino Decaroli <caronte.ade@gmail.com>
2026-09-13 23:23:49 +0200

    Promote VETUSTUS Micro to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "mobi.vxd.vetustus.micro"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0-beta.2"
+        versionCode = 4
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
