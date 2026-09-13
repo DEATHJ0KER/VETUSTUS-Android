1f25a2e8a109f46444cdfcd54437ac61f13f8d3b
Valentino Decaroli <caronte.ade@gmail.com>
2026-09-13 22:42:58 +0200

    Bump VETUSTUS Micro to 0.2.0 Beta 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "mobi.vxd.vetustus.micro"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-beta.1"
+        versionCode = 3
+        versionName = "0.2.0-beta.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
