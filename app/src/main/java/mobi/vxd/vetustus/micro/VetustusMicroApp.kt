b30c105e3a5c2f28ce8d7e690aee0e004d27ccb5
Valentino Decaroli <caronte.ade@gmail.com>
2026-09-16 21:33:56 +0200

    Wire app-open ads into application lifecycle

--- app/src/main/java/mobi/vxd/vetustus/micro/VetustusMicroApp.kt
+++ app/src/main/java/mobi/vxd/vetustus/micro/VetustusMicroApp.kt
@@ -1,10 +1,16 @@
 package mobi.vxd.vetustus.micro
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import mobi.vxd.vetustus.micro.ads.AppOpenAdManager
 import mobi.vxd.vetustus.micro.data.AppDatabase
 import mobi.vxd.vetustus.micro.data.DownloadRepository
 import mobi.vxd.vetustus.micro.data.LibraryRepository
@@ -15,15 +21,59 @@ import mobi.vxd.vetustus.micro.registration.FirstRunRegistration
 import mobi.vxd.vetustus.micro.storage.MediaArchiveManager
 import mobi.vxd.vetustus.micro.storage.MediaStorePublisher
 
-class VetustusMicroApp : Application() {
+class VetustusMicroApp :
+    Application(),
+    Application.ActivityLifecycleCallbacks,
+    DefaultLifecycleObserver {
+
     val container: AppContainer by lazy { AppContainer(this) }
     private val appScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var currentActivity: Activity? = null
 
     override fun onCreate() {
-        super.onCreate()
+        super<Application>.onCreate()
+        registerActivityLifecycleCallbacks(this)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+
         container.database.recoverInterruptedTransfers()
         container.downloads.refresh()
         appScope.launch { container.registration.registerIfNeeded() }
+    }
+
+    override fun onStart(owner: LifecycleOwner) {
+        currentActivity?.let(container.ads::onAppForeground)
+    }
+
+    override fun onStop(owner: LifecycleOwner) {
+        container.ads.onAppBackground()
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        currentActivity = activity
+        container.ads.updateCurrentActivity(activity)
+        container.ads.initializeConsent(activity)
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        if (!container.ads.isShowingAd) {
+            currentActivity = activity
+            container.ads.updateCurrentActivity(activity)
+        }
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        if (!container.ads.isShowingAd) {
+            currentActivity = activity
+            container.ads.updateCurrentActivity(activity)
+        }
+    }
+
+    override fun onActivityPaused(activity: Activity) = Unit
+    override fun onActivityStopped(activity: Activity) = Unit
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+
+    override fun onActivityDestroyed(activity: Activity) {
+        if (currentActivity === activity) currentActivity = null
     }
 }
 
@@ -37,4 +87,5 @@ class AppContainer(application: Application) {
     val publisher = MediaStorePublisher(application)
     val archiveManager = MediaArchiveManager(application, publisher, library)
     val registration = FirstRunRegistration(application)
+    val ads = AppOpenAdManager(application)
 }
